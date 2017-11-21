a03df86347494964f912654dbd006cdfa668522a
eggyy1224 <eggyy1224@gmail.com>
2017-11-21 10:44:24 +0800

    Implememt advanced login

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,10 +1,12 @@
 class UsersController < ApplicationController
   def new
     @user = User.new
+    
   end
 
   def create
     @user = User.new(user_params)
+
     if @user.save
       log_in @user
       flash[:success] = "Welcome to the sample app"
--- app/models/user.rb
+++ app/models/user.rb
@@ -1,4 +1,6 @@
 class User < ApplicationRecord
+  attr_accessor :remember_token
+
   before_save { self.email = self.email.downcase }
   validates :name, presence: true, length: { maximum: 50 }
 
@@ -11,10 +13,28 @@ class User < ApplicationRecord
   has_secure_password
 
   # 返回指定字符串的哈希摘要
-  def User.digest(string)
+  def User.digest(string)#class method
     cost = ActiveModel::SecurePassword.min_cost ? BCrypt::Engine::MIN_COST :
                                                   BCrypt::Engine.cost
     BCrypt::Password.create(string, cost: cost)
+  end
+
+  def User.new_token#class method
+    SecureRandom.urlsafe_base64
+  end
+
+  def remember
+    self.remember_token = User.new_token
+    update_attribute(:remember_digest, User.digest(remember_token))
+  end
+
+  def authenticated?(remember_token)
+    return false if remember_digest.nil?
+    BCrypt::Password.new(remember_digest).is_password?(remember_token)
+  end
+
+  def forget
+    update_attribute(:remember_digest, nil)
   end
 
 end
