9863fb8d1096a7f220274227301190a25dda0b1b
eggyy1224 <eggyy1224@gmail.com>
2017-11-22 10:25:18 +0800

    finish user edit, update, index, and destroy actions

--- app/models/user.rb
+++ app/models/user.rb
@@ -9,7 +9,7 @@ class User < ApplicationRecord
                     format: { with: VALID_EMAIL_REGEX },
                     uniqueness: { case_sensitive: false }
 
-  validates :password, presence: true, length: { minimum: 6 }
+  validates :password, presence: true, length: { minimum: 6 }, allow_nil: true
   has_secure_password
 
   # 返回指定字符串的哈希摘要
