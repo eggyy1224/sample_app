098b117d843d34333d7391d3c3e8f0a4f3d5ad56
eggyy1224 <eggyy1224@gmail.com>
2017-11-27 09:41:14 +0800

    Add user microposts

--- app/uploaders/picture_uploader.rb
+++ app/uploaders/picture_uploader.rb
@@ -10,7 +10,11 @@ class PictureUploader < CarrierWave::Uploader::Base
 
   # Choose what kind of storage to use for this uploader:
   
-  storage :file
+  if Rails.env.production?
+    storage :fog
+  else
+    storage :file
+  end
   
   # storage :fog
 
